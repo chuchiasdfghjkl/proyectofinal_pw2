03eed3c62426f025f13e09dc03623b6ffe01e851
GeovannyB <geovannypinto356@gmail.com>
2024-04-13 14:54:52 -0600

    Agregando pagina Productos

--- context/productosPage.js
+++ context/productosPage.js
@@ -1,19 +1,34 @@
 export default {
     productos:[
         {
-            "url": "https://picsum.photos/id/832/150/150",
-            "titulo":"John Doe",
-            "des":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+            "url": "/xbox.jpg",
+            "titulo":"Gift Cards Xbox",
+            "des":"Tenemos Xbox Gift Cards disponibles, con ellas puedes adquirir juegos, comprar tu membresía de Game Pass y mucho más."
         },
         {
-            "url": "https://picsum.photos/id/823/150/150",
-            "titulo": "Jane Doe",
-            "des":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+            "url": "/play.png",
+            "titulo": "Gift Cards Play Station",
+            "des":"Tenemos PlayStation Gift Cards disponibles, con ellas puedes adquirir juegos, comprar tu membresía de PlayStation Plus y mucho más. "
         },
         {
-            "url": "https://picsum.photos/id/680/150/150",
-            "titulo":"Junior Doe",
-            "des":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+            "url": "/nintendo.jpg",
+            "titulo":"Gift Cards Nintendo",
+            "des":"Adquiere tus Gift Cards Nintendo y compra tus personajes favoritos, desbloquea mundos y suscribete a Nintendo Switch Online."
+        },
+        {
+            "url": "/spotify.jpg",
+            "titulo": "Cuentas Spotify Premium",
+            "des":"Obten tu cuenta por tan solo L.90 al mes. Deposita a cualquiera de nuestras cuentas y nosotros te brindaremos tu código."
+        },
+        {
+            "url": "/apple.jpeg",
+            "titulo":"Gift Cards Apple",
+            "des":"Compra productos, accesorios, apps, juegos, música, peliculas, series, iCloud+ y muchos más."
+        },
+        {
+            "url": "/roblox.jpg",
+            "titulo":"Gift Cards Roblox",
+            "des":"Compra productos, accesorios y muchos más con las Gift Cards de Roblox."
         }
     ]
 }
